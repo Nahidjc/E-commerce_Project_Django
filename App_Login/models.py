8a749b0cd057c8ef5554f7a0626239c138128e64
Nahid2194 <nahidjc80@gmail.com>
2021-01-23 21:42:12 +0600

    set model for User

--- App_Login/models.py
+++ App_Login/models.py
@@ -21,6 +21,35 @@ class MyUserManager(BaseUserManager):
         extra_fields.setdefault('is_active', True)
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff = True')
-        f extra_fields.get('is_superuser') is not True:
+        if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser = True')
         return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, null=False)
+    is_staff = models.BooleanField(
+        ugettext_lazy('staff status'),
+        default=False,
+        help_text=ugettext_lazy('Designates whether the user can log in this site'
+                                )
+    )
+    is_active = models.BooleanField{
+        ugettext_lazy('active'),
+        default = True,
+        help_text = ugettext_lazy(
+            'Designates whether this user should be treated as active. Unselect this instead of deleting accounts')
+
+    }
+
+    USERNAME_FIELD = 'email'
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
