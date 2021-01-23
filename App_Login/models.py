2cf4fc77c32c33dc3af0e35bc8f92e00c94b3314
Nahid2194 <nahidjc80@gmail.com>
2021-01-23 21:18:45 +0600

    set models for normal user

--- App_Login/models.py
+++ App_Login/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy
 # Create your models here.
+
+
+class MyUserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The email must be set!")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
