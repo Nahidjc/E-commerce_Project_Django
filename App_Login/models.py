30d51226949846ef279ab05cc357c5dcf7dc814d
Nahid2194 <nahidjc80@gmail.com>
2021-01-23 21:27:52 +0600

    set model for superuser

--- App_Login/models.py
+++ App_Login/models.py
@@ -14,3 +14,13 @@ class MyUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff = True')
+        f extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser = True')
+        return self._create_user(email, password, **extra_fields)
