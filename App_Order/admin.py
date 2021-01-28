13801b935b858d1d7216f0be8dc141ed20f125d3
Nahid2194 <nahidjc80@gmail.com>
2021-01-28 09:20:21 +0600

    Adding models in App_Order

--- App_Order/admin.py
+++ App_Order/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from App_Order.models import Order, Cart
 
 # Register your models here.
+admin.site.register(Order)
+admin.site.register(Cart)
--- App_Order/models.py
+++ App_Order/models.py
@@ -1,3 +1,39 @@
 from django.db import models
-
+from django.conf import settings
+from App_Shop.models import Product
 # Create your models here.
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name="cart")
+    item = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="cart")
+    quantity = models.IntegerField(default=1)
+    purchased = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.quantity} X {self.item}'
+
+    def get_total(self):
+        total = self.item * self.quantity
+        float_total = format(total, '0.2f')
+        return float_total
+
+
+class Order(models.Model):
+    orderitems = models.ManyToManyField(Cart)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    paymentId = models.CharField(max_length=264, blank=True, null=True)
+    ordereId = models.CharField(max_length=200, blank=True, null=True)
+
+    def get_totals(self):
+        total = 0
+        for order_item in self.orderitems.all():
+            total += float(order_item.get_total())
+        return total
