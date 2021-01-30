742ef26cd115fee6ddc77cd096fbc9cccccb6303
Nahid2194 <nahidjc80@gmail.com>
2021-01-30 21:13:49 +0600

    remove item

--- App_Order/urls.py
+++ App_Order/urls.py
@@ -6,4 +6,5 @@ app_name = 'App_Order'
 urlpatterns = [
     path('add/<pk>/', views.add_to_cart, name="add"),
     path('cart/', views.cart_view, name="cart"),
+    path('remove/<pk>/', views.remove_from_cart, name="remove"),
 ]
--- App_Order/views.py
+++ App_Order/views.py
@@ -42,3 +42,24 @@ def cart_view(request):
     else:
         messages.warning(request, "You don't have any item in your cart!")
         return redirect("App_Shop:home")
+
+
+@login_required
+def remove_from_cart(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            order_item = Cart.objects.filter(
+                item=item, user=request.user, purchased=False)[0]
+            order.orderitems.remove(order_item)
+            order_item.delete()
+            messages.warning(request, "This item was removed from your cart")
+            return redirect("App_Order:cart")
+        else:
+            messages.info(request, "This item was not in your cart.")
+            return redirect("App_Shop:home")
+    else:
+        messages.info(request, "You don't have an active order")
+        return redirect("App_Shop:home")
