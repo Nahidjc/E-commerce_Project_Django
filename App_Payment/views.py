a6ca873c03702eef134ddfe7e2566dcd42e6e17d
Nahid2194 <nahidjc80@gmail.com>
2021-02-01 22:55:24 +0600

    Payment App checkout views file update

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def checkout(request):
     saved_address = BillingAddress.objects.get_or_create(user=request.user)
+    saved_address = saved_address[0]
     form = BillingForm(instance=saved_address)
     if request.method == 'POST':
         form = BillingForm(request.POST, instance=saved_address)
