e1e40ea10a1aa86aa55041a75993ef937fbe9950
Nahid2194 <nahidjc80@gmail.com>
2021-02-01 22:49:48 +0600

    Payment App checkout form send template from views

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -11,4 +11,9 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def checkout(request):
     saved_address = BillingAddress.objects.get_or_create(user=request.user)
-    return render(request, 'App_Payment/checkout.html', context={'saved_address': saved_address})
+    form = BillingForm(instance=saved_address)
+    if request.method == 'POST':
+        form = BillingForm(request.POST, instance=saved_address)
+        if form.is_valid():
+            form = BillingForm(instance=saved_address)
+    return render(request, 'App_Payment/checkout.html', context={'form': form})
