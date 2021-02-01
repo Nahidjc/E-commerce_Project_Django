187503afec481e71a9225e504f81c0f85d8b6caa
Nahid2194 <nahidjc80@gmail.com>
2021-02-01 22:31:28 +0600

    Payment App view setup

--- App_Payment/urls.py
+++ App_Payment/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from App_Payment import views
 app_name = 'App_Payment'
 urlpatterns = [
-
+    path('checkout/', views.checkout, name='checkout'),
 ]
--- App_Payment/views.py
+++ App_Payment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
+from App_Order.models import Order
+from App_Payment.forms import BillingForm
+from App_Payment.models import BillingAddress
+
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def checkout(request):
+    saved_address = BillingAddress.objects.get_or_create(user=request.user)
+    return render(request, 'App_Payment/checkout.html', context={'saved_address': saved_address})
