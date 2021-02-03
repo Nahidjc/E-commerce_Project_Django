43e8665d487eb2067bff8781c9a8e39332918aa5
Nahid2194 <nahidjc80@gmail.com>
2021-02-03 21:59:10 +0600

    payment details showing ssl card

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -33,7 +33,8 @@ def checkout(request):
 @login_required
 def payment(request):
     saved_address = BillingAddress.objects.get_or_create(user=request.user)
-    if not saved_address[0].is_fully_filled():
+    saved_address = saved_address[0]
+    if not saved_address.is_fully_filled():
         messages.info(request, f"Please complete shopping address!")
         return redirect("App_Payment:checkout")
     if not request.user.profile.is_fully_filled():
@@ -48,7 +49,24 @@ def payment(request):
     print(status_url)
     mypayment.set_urls(success_url=status_url, fail_url=status_url,
                        cancel_url=status_url, ipn_url=status_url)
-    return render(request, "App_Payment/payment.html", context={})
+
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    order_items = order_qs[0].orderitems.all()
+    order_items_count = order_qs[0].orderitems.count()
+    order_total = order_qs[0].get_totals()
+    mypayment.set_product_integration(total_amount=Decimal(order_total), currency='BDT', product_category='Mixed',
+                                      product_name=order_items, num_of_item=order_items_count, shipping_method='Courier', product_profile='None')
+
+    current_user = request.user
+    mypayment.set_customer_info(name='John Doe', email='johndoe@email.com', address1=current_user.profile.address_1, address2=current_user.profile.address_1,
+                                city=current_user.profile.city,  postcode=current_user.profile.zipcode, country=current_user.profile.country, phone=current_user.profile.phone)
+    mypayment.set_shipping_info(shipping_to=current_user.profile.full_name, address=saved_address.address,
+                                city=saved_address.city, postcode=saved_address.zipcode, country=saved_address.country)
+
+
+response_data = mypayment.init_payment()
+
+return render(request, "App_Payment/payment.html", context={})
 
 
 @login_required
